4334fd784488b51dde61d52b89031d89afe07542
Sshivam <shivamsingin@gmail.com>
2025-09-01 16:05:07 +0530

    Fix React router base path for GitHub Pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, Router as WouterRouter } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -22,24 +22,26 @@ import NotFound from "@/pages/not-found";
 
 function Router() {
   return (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route path="/services" component={Services} />
-      <Route path="/local-seo" component={LocalSEO} />
-      <Route path="/ecommerce-seo" component={EcommerceSEO} />
-      <Route path="/enterprise-seo" component={EnterpriseSEO} />
-      <Route path="/youtube-seo" component={YouTubeSEO} />
-      <Route path="/ppc-advertising" component={PPCAdvertising} />
-      <Route path="/social-media-marketing" component={SocialMediaMarketing} />
-      <Route path="/web-development" component={WebDevelopment} />
-      <Route path="/email-marketing" component={EmailMarketing} />
-      <Route path="/content-marketing" component={ContentMarketing} />
-      <Route path="/mobile-marketing" component={MobileMarketing} />
-      <Route path="/about" component={About} />
-      <Route path="/case-studies" component={CaseStudies} />
-      <Route path="/contact" component={Contact} />
-      <Route component={NotFound} />
-    </Switch>
+    <WouterRouter base="/digital-tech-guides">
+      <Switch>
+        <Route path="/" component={Home} />
+        <Route path="/services" component={Services} />
+        <Route path="/local-seo" component={LocalSEO} />
+        <Route path="/ecommerce-seo" component={EcommerceSEO} />
+        <Route path="/enterprise-seo" component={EnterpriseSEO} />
+        <Route path="/youtube-seo" component={YouTubeSEO} />
+        <Route path="/ppc-advertising" component={PPCAdvertising} />
+        <Route path="/social-media-marketing" component={SocialMediaMarketing} />
+        <Route path="/web-development" component={WebDevelopment} />
+        <Route path="/email-marketing" component={EmailMarketing} />
+        <Route path="/content-marketing" component={ContentMarketing} />
+        <Route path="/mobile-marketing" component={MobileMarketing} />
+        <Route path="/about" component={About} />
+        <Route path="/case-studies" component={CaseStudies} />
+        <Route path="/contact" component={Contact} />
+        <Route component={NotFound} />
+      </Switch>
+    </WouterRouter>
   );
 }
 
